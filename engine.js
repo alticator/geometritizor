e3704afa6368b9b98082adb7ddbb94402318b7b2
alticator <main@alticator.com>
2021-04-10 09:46:35 +0300

    Fix Decimal Issues
    Fix decimal bugs in the Geometritizor Engine. Closes #3

--- engine.js
+++ engine.js
@@ -3,17 +3,13 @@
 
 var engine = {
     multiply: function(a, b) {
-        var first = a * 1000000000000;
-        var second = b * 1000000000000;
-        var multiplication = first * second;
-        var result = multiplication / 1000000000000000000000000;
+        var correctionFactor = 10;
+        var result = (a * correctionFactor) * (b * correctionFactor) / (correctionFactor * correctionFactor);
         return result;
     },
     add: function(a, b) {
-        var first = a * 1000000000000;
-        var second = b * 1000000000000;
-        var addition = first + second;
-        var result = addition / 1000000000000;
+        var correctionFactor = 10;
+        var result = ((a * correctionFactor) + (b * correctionFactor)) / (correctionFactor);
         return result;
     }
 }
